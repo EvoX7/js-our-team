91adfed29af68656d7ec6d6aad6bd8beb47403c0
EvoX7 <creativesoultouch@gmail.com>
2022-06-28 17:25:25 +0200

    Bonus 1 - 2

--- js/main.js
+++ js/main.js
@@ -13,47 +13,64 @@
 const companyEmployeesList = [{
     name: 'Wayne Barnett',
     role: 'Founder & CEO',
-    photo: 'wayne-barnett-founder-ceo.jpg',
+    photo: './img/wayne-barnett-founder-ceo.jpg',
 
 }, {
     name: 'Angela Caroll',
     role: 'Chief Editor',
-    photo: 'angela-caroll-chief-editor.jpg',
+    photo: './img/angela-caroll-chief-editor.jpg',
 
 }, {
     name: 'Walter Gordon',
     role: 'Office Manager',
-    photo: 'walter-gordon-office-manager.jpg',
+    photo: './img/walter-gordon-office-manager.jpg',
 
 }, {
     name: 'Angela Lopez',
     role: 'Social Media Manager',
-    photo: 'angela-lopez-social-media-manager.jpg',
+    photo: './img/angela-lopez-social-media-manager.jpg',
 
 }, {
     name: 'Scott Estrada',
     role: 'Developer',
-    photo: 'scott-estrada-developer.jpg',
+    photo: './img/scott-estrada-developer.jpg',
 
 }, {
     name: 'Barbara Ramos',
     role: 'Graphic Designer',
-    photo: 'barbara-ramos-graphic-designer.jpg',
+    photo: './img/barbara-ramos-graphic-designer.jpg',
 }];
+
+// * Defining DOM output 
 
 const outputList = document.getElementById('list-output');
 
 // ? Cycling through the list of employees and show info on the consle 
-
 for (let i = 0; i < companyEmployeesList.length; i++) {
-
     const listVisual = companyEmployeesList[i];
     console.log(`    ${listVisual.name} 
     ${listVisual.role} 
     ${listVisual.photo}`);
-    outputList.innerHTML += `<p>${listVisual.name} <br> ${listVisual.role} <br> ${listVisual.photo}</p>`;
-    
+
+    const ListItem = document.createElement('div');
+    ListItem.innerHTML = `<div class="card m-3 " style="width: 18rem;">
+      <img src="${listVisual['photo']}" class="card-img-top" alt="...">
+      <div class="card-body ">
+        <h5 class="card-title fw-bold">Employee Card</h5>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">${listVisual.name}</li>
+        <li class="list-group-item fw-bold">${listVisual.role}</li>
+      </ul>
+    </div>
+`;
+    outputList.append(ListItem);
 }
 
+
+
+
+// * Milestone 2 output
+// outputList.innerHTML += `<p>${listVisual.name} <br> ${listVisual.role} <br> ${listVisual.photo}</p>`;
 
 
